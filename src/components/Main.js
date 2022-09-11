0354872eff74d09867a2c078136a4647770925c5
rajatbhatia1998 <rajat.bhatia859@gmail.com>
2022-09-11 13:26:13 +0530

    all fixes

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,14 @@ import About from './About'
 import Navbar from './Navbar'
 import Skills from './Skills'
 import {getSiteData} from '../firebase'
-
+import './custom/loader.css'
 
 export default function Main() {
 
   const [totalExp,setTotalExp] = useState(0)
-
+  const [isLoading , setLoading] = useState(true)
   useEffect(()=>{
+   
     getSiteData().then(data=>{
       console.log("FIREBASE SITEDATA",data)
       const exp = data.find(d=>d.type ==='experience-start')
@@ -25,12 +26,26 @@ export default function Main() {
     var diff = nowDate.getTime() - startDate.getTime(); 
     const exp =  (diff / 31536000000).toFixed(1); 
     setTotalExp(exp)
+    setLoading(false)
     }
   return (
+    <>
+    {isLoading ?
+
+      <div className='container flex justify-center items-center' style={{height:'500px'}}>
+      
+      <div class="lds-circle"><div></div></div>
+      
+      </div>
+   
+   :
     <div>
+   
         <Navbar/>
         <About totalExp={totalExp}/>
         <Skills totalExp={totalExp}/>
     </div>
+    }
+    </>
   )
 }
