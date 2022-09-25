530dfa3a09bee1bf0c43868cf5030e1b23852df1
rajatbhatia1998 <rajat.bhatia859@gmail.com>
2022-09-25 18:21:53 +0530

    project

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,10 +4,6 @@ export default function Project() {
   return (
     <div className='bg-blue-700 mt-10' style={{width:'100%',height:'auto',paddingBottom:'50px'}}  >
         
-               
-
-   
-
-</div>
+    </div>
   )
 }
